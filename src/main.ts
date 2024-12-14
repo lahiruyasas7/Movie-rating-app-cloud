d644b9e30ba2634b3f7aecb711d045b06b4a72cd
Lahiru Yasas <lahiruyasas7@gmail.com>
2024-12-14 15:39:05 +0530

    added swagger and enable cores

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { setupSwagger } from './configs/swager-config/swagger';
+import bodyParser from 'body-parser';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.setGlobalPrefix(process.env.API_PREFIX, {
+    exclude: ['/'],
+  });
+  app.useGlobalPipes(new ValidationPipe());
+  const configService = app.get(ConfigService);
+  //cors configuration
+  app.enableCors({
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    credentials: true,
+    origin: '*',
+  });
+
+   // Setup Swagger
+   setupSwagger(app);
+   app.use(bodyParser.json({ limit: '5000mb' }));
+   app.use(bodyParser.urlencoded({ limit: '5000mb', extended: true }));
+   app.use(cookieParser());
+   await app.listen(configService.get('app.port'), () => {
+     console.log(`Server running at port: ${configService.get('app.port')}`);
+   });
 }
 bootstrap();
