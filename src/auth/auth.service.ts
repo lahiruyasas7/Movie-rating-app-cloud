8e20b15f0f5ee031e4dd50862901b5f8871af8b2
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-01-13 22:46:44 +0530

    created register service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,83 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  Logger,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from 'src/entities/user.entity';
+import { EntityManager, Repository } from 'typeorm';
+import { RegisterUserDto } from './dto/register-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  
-}
+  private logger: Logger = new Logger(AuthService.name);
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+    private readonly entityManager: EntityManager,
+  ) {}
 
+  ///////////////////  Student Registration //////////////////////////
+
+  async register(registerUserDto: RegisterUserDto) {
+    try {
+      this.logger.debug('register user');
+      const {
+        email,
+        password,
+        firstName,
+        lastName,
+        phone,
+        //profileImage,
+        address,
+        dateOfBirth,
+      } = registerUserDto;
+
+      // hash password using bcrypt
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // Check existing user
+      const existingUser = await this.userRepository.findOne({
+        where: { email },
+      });
+      if (existingUser) {
+        throw new ConflictException('Email is already in use');
+      }
+
+      // create new user
+      const newUser = this.userRepository.create({
+        email,
+        password: hashedPassword,
+        firstName,
+        lastName,
+        phone,
+
+        //profileImage,
+        address,
+        dateOfBirth,
+      });
+
+      await this.userRepository.save(newUser);
+
+      return newUser;
+    } catch (error) {
+      this.logger.error(`register ${error}`);
+
+      // conflict errors
+      if (error instanceof ConflictException) {
+        throw error;
+      }
+      // catch errors
+      throw new UnauthorizedException('Failed to register new user');
+    }
+  }
+}
 @Injectable()
 export class JWTAuthService {
   constructor(
@@ -22,4 +91,3 @@ export class JWTAuthService {
     });
   }
 }
-
