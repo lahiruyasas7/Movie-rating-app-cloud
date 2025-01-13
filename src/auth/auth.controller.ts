36bbd2848e0334c6eee69b944983fbb4c736f0b7
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-01-13 23:04:45 +0530

    created controller for register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,35 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import {
+  ApiConsumes,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { RegisterUserDto } from './dto/register-user.dto';
 
+@ApiTags('Auth') // swagger tag
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  ////////// register ///////////
+  @ApiOperation({ summary: 'Register a new User' }) // Operation summary swagger
+  @ApiResponse({ status: 201, description: '' }) // api response swagger
+  @ApiResponse({ status: 409, description: 'Email is already in use' }) // api response swagger
+  //@ApiConsumes('form-data')
+  @Post('register')
+  register(@Body() registerUserDto: RegisterUserDto) {
+    return this.authService.register(registerUserDto);
+  }
 }
