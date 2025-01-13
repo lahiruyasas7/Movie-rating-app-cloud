4f6e837a87115fcd7be05de6f4ff6460011cb140
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-01-13 22:21:22 +0530

    removed sample end points

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,25 +6,7 @@ import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
-  }
-
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
+  
 }
 
 @Injectable()
