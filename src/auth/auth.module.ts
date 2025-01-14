788fb51eccab128d5204cd5ef94f5824e4dad48d
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-01-14 09:51:37 +0530

    added jwtModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { AuthService, JWTAuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from 'src/entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [ JwtModule.registerAsync({
+    inject: [ConfigService], // Inject ConfigService
+    useFactory: (configService: ConfigService) => ({
+      secret: configService.get<string>('app.jwtSecret'),
+      signOptions: { expiresIn: '24h' },
+    }),
+  }),TypeOrmModule.forFeature([UserEntity])],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JWTAuthService],
 })
 export class AuthModule {}
