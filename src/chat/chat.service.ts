20a3a1176c38fa111392173278d720865367fe7b
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-07-13 18:03:09 +0530

    created services for get messages ans save messages

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,26 +1,27 @@
 import { Injectable } from '@nestjs/common';
-import { CreateChatDto } from './dto/create-chat.dto';
-import { UpdateChatDto } from './dto/update-chat.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ChatEntity } from 'src/entities/chat.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ChatService {
-  create(createChatDto: CreateChatDto) {
-    return 'This action adds a new chat';
+  constructor(
+    @InjectRepository(ChatEntity)
+    private chatRepository: Repository<ChatEntity>,
+  ) {}
+
+  async saveMessage(senderId: string, receiverId: string, content: string) {
+    const msg = this.chatRepository.create({ senderId, receiverId, content });
+    return await this.chatRepository.save(msg);
   }
 
-  findAll() {
-    return `This action returns all chat`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} chat`;
-  }
-
-  update(id: number, updateChatDto: UpdateChatDto) {
-    return `This action updates a #${id} chat`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} chat`;
+  async getMessages(userId1: string, userId2: string) {
+    return this.chatRepository.find({
+      where: [
+        { senderId: userId1, receiverId: userId2 },
+        { senderId: userId2, receiverId: userId1 },
+      ],
+      order: { createdAt: 'ASC' },
+    });
   }
 }
