3f491d1c60b90d4a3da4c72afc5ad657332e2962
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-07-23 23:54:25 +0530

    added sample comment to check cicd work flow

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+//sample change to check cicd flow working
   @Get()
   getHello(): string {
     return this.appService.getHello();
