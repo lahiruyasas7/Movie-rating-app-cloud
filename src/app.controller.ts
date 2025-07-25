099243b9f457f1074fa358c5a48f6bdb704b3cde
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-07-25 23:23:32 +0530

    sample change 3 to test cicd

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,6 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 //sample change to check cicd flow working
 //sample chnage 2 to check cicd flow working
+//sample change 3 to check cicd flow working
   @Get()
   getHello(): string {
     return this.appService.getHello();
