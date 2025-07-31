6ddce52361d720bb6d6113f9782ed265dcfbee79
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-07-31 21:45:39 +0530

    feat: configured rate limit

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,12 +8,30 @@ import { DatabaseModule } from './configs/database-config/database.module';
 import { ChatModule } from './chat/chat.module';
 import appConfig from './configs/app-configs/app.config';
 import { ConfigModule } from '@nestjs/config';
+import { minutes, ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [AuthModule, AppConfigModule, DatabaseModule, ChatModule, ConfigModule.forRoot({
+  imports: [
+    AuthModule,
+    AppConfigModule,
+    DatabaseModule,
+    ChatModule,
+    ConfigModule.forRoot({
       load: [appConfig],
-    }),],
+    }),
+    ThrottlerModule.forRoot([
+      {
+        name: 'default', // If name is not provided, the name is given as default
+        ttl: minutes(1), // Time window in minutes
+        limit: 10, // Number of allowed requests in that window
+      },
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },],
 })
 export class AppModule {}
