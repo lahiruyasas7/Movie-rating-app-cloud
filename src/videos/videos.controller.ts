e3043d39f98ebfc6ddef8db8c99dd137dde9a6e4
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-08-05 23:57:55 +0530

    feat: add video upload endpoint with metadata via Multer interceptor

--- src/videos/videos.controller.ts
+++ src/videos/videos.controller.ts
@@ -1,10 +1,22 @@
 import { InjectQueue } from '@nestjs/bullmq';
-import { Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { Queue } from 'bullmq';
+import { CreateVideoDto } from './dto/create-video.dto';
+import { VideoService } from './videos.service';
 
 @Controller('videos')
 export class VideosController {
-  constructor(@InjectQueue('video') private readonly videoQueue: Queue) {}
+  constructor(
+    @InjectQueue('video') private readonly videoQueue: Queue,
+    private readonly videoService: VideoService,
+  ) {}
 
   @Post('process')
   async processVideo() {
@@ -15,5 +27,14 @@ export class VideosController {
     return {
       message: 'Video processing job added to the queue',
     };
+  }
+
+  @Post('add')
+  @UseInterceptors(FileInterceptor('video'))
+  async uploadVideo(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() dto: CreateVideoDto,
+  ) {
+    return this.videoService.createVideo(dto, file);
   }
 }
