d32adb9e75c81201dccc62b87d0c0cc299a8eea7
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-08-05 23:48:18 +0530

    feat: created service for created video endpoint

---
+++ src/videos/videos.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { InjectQueue } from '@nestjs/bullmq';
+import { Queue } from 'bullmq';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateVideoDto } from './dto/create-video.dto';
+import { Video } from 'src/entities/video.entity';
+import { UploadService } from 'src/util/uploadTos3.service';
+
+
+@Injectable()
+export class VideoService {
+  constructor(
+    @InjectRepository(Video)
+    private readonly videoRepo: Repository<Video>,
+    private readonly uploadService: UploadService,
+    @InjectQueue('video-processing')
+    private readonly videoQueue: Queue,
+  ) {}
+
+  async createVideo(dto: CreateVideoDto, file: Express.Multer.File) {
+    const s3Url = await this.uploadService.uploadVideo(file);
+
+    const video = this.videoRepo.create({
+      name: dto.name,
+      description: dto.description,
+      s3Url,
+    });
+
+    const saved = await this.videoRepo.save(video);
+
+    // Enqueue background job
+    await this.videoQueue.add('process-video', {
+      videoId: saved.id,
+      s3Url,
+    });
+
+    return saved;
+  }
+}
