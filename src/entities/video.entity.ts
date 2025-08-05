9472adf1844defecc6a82a78e0ceb83e12e4cc4f
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-08-05 14:28:44 +0530

    feat: created entity for videos

---
+++ src/entities/video.entity.ts
@@ -0,0 +1,22 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('videos')
+export class Video {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ type: 'text' })
+  description: string;
+
+  @Column()
+  s3Url: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
