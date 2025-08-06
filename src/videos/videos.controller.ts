7e7f919fa706ef7b59e7f1b7f1bda8ac70e27a18
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-08-06 23:48:41 +0530

    feat: added userId as param

--- src/videos/videos.controller.ts
+++ src/videos/videos.controller.ts
@@ -2,8 +2,11 @@ import { InjectQueue } from '@nestjs/bullmq';
 import {
   Body,
   Controller,
+  Param,
   Post,
+  Request,
   UploadedFile,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
@@ -11,6 +14,8 @@ import { Queue } from 'bullmq';
 import { CreateVideoDto } from './dto/create-video.dto';
 import { VideoService } from './videos.service';
 import { ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from 'src/auth/auth.guard';
+//import { AuthGuard } from '@nestjs/passport';
 
 @ApiTags('Videos')
 @Controller('videos')
@@ -20,23 +25,25 @@ export class VideosController {
     private readonly videoService: VideoService,
   ) {}
 
-//   @Post('process')
-//   async processVideo() {
-//     await this.videoQueue.add('process', {
-//       fileName: 'best-video',
-//       fileType: 'mp4',
-//     });
-//     return {
-//       message: 'Video processing job added to the queue',
-//     };
-//   }
+  //   @Post('process')
+  //   async processVideo() {
+  //     await this.videoQueue.add('process', {
+  //       fileName: 'best-video',
+  //       fileType: 'mp4',
+  //     });
+  //     return {
+  //       message: 'Video processing job added to the queue',
+  //     };
+  //   }
 
-  @Post('add')
+  @Post('add:userId')
+  @UseGuards(AuthGuard)
   @UseInterceptors(FileInterceptor('video'))
   async uploadVideo(
     @UploadedFile() file: Express.Multer.File,
     @Body() dto: CreateVideoDto,
+    @Param('userId') userId: string,
   ) {
-    return this.videoService.createVideo(dto, file);
+    return this.videoService.createVideo(dto, file, userId);
   }
 }
