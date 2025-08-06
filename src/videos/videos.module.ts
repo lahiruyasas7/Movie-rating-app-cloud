f2cfe84d7db74469708221e13831ad7c6af0630e
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-08-06 11:51:24 +0530

    feat: created video module

---
+++ src/videos/videos.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BullModule } from '@nestjs/bullmq';
+import { Video } from 'src/entities/video.entity';
+import { VideosController } from './videos.controller';
+import { VideoService } from './videos.service';
+import { VideosProcessor } from './videos.worker';
+import { UploadService } from 'src/util/uploadTos3.service';
+import { VideoQueueEventsListener } from './videos-queue.event';
+import { VideoProcessor } from './video.processor';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Video]),
+    BullModule.registerQueue({
+      name: 'video-processing',
+    }),
+  ],
+  controllers: [VideosController],
+  providers: [
+    VideoService,
+    VideoProcessor,
+    UploadService,
+    //VideoQueueEventsListener,
+  ],
+})
+export class VideoModule {}
